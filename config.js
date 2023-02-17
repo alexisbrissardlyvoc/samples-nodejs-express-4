ee614f84a534357f1eca2ea3e61fa9bc73632e30
alexisbrissardlyvoc <alexis.brissard@lyvoc.com>
2023-02-17 10:24:51 +0100

    add(heroku config)

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const dotenv = require('dotenv');
-const fs = require('fs');
+const path = require("path");
+const dotenv = require("dotenv");
+const fs = require("fs");
 
 // Read environment variables from "testenv". Override environment vars if they are already set. https://www.npmjs.com/package/dotenv
-const TESTENV = path.resolve(__dirname, 'testenv');
+const TESTENV = path.resolve(__dirname, "testenv");
 if (fs.existsSync(TESTENV)) {
   const envConfig = dotenv.parse(fs.readFileSync(TESTENV));
   Object.keys(envConfig).forEach((k) => {
@@ -11,11 +11,14 @@ if (fs.existsSync(TESTENV)) {
   });
 }
 
-var ISSUER = process.env.ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
-var CLIENT_ID = process.env.CLIENT_ID || '{clientId}';
-var CLIENT_SECRET = process.env.CLIENT_SECRET || '{clientSecret}';
-var SPA_CLIENT_ID = process.env.SPA_CLIENT_ID || '{spaClientId}';
-var OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK ? true : false;
+var ISSUER =
+  process.env.ISSUER || "https://{yourOktaDomain}.com/oauth2/default";
+var CLIENT_ID = process.env.CLIENT_ID || "{clientId}";
+var CLIENT_SECRET = process.env.CLIENT_SECRET || "{clientSecret}";
+var SPA_CLIENT_ID = process.env.SPA_CLIENT_ID || "{spaClientId}";
+var OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK
+  ? true
+  : false;
 
 module.exports = {
   webServer: {
@@ -24,11 +27,11 @@ module.exports = {
       clientId: CLIENT_ID,
       clientSecret: CLIENT_SECRET,
       issuer: ISSUER,
-      appBaseUrl: 'http://localhost:8080',
-      scope: 'openid profile email groupadmin offline_access',
+      appBaseUrl: "http://ec2-13-38-87-73.eu-west-3.compute.amazonaws.com:8080",
+      scope: "openid profile email groupadmin offline_access",
       testing: {
-        disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK
-      }
+        disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
+      },
     },
   },
   resourceServer: {
@@ -37,12 +40,12 @@ module.exports = {
       clientId: SPA_CLIENT_ID,
       issuer: ISSUER,
       testing: {
-        disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK
-      }
+        disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
+      },
     },
     assertClaims: {
-      aud: 'api://default',
-      cid: SPA_CLIENT_ID
-    }
-  }
+      aud: "api://default",
+      cid: SPA_CLIENT_ID,
+    },
+  },
 };
